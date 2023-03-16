8b39cf1d13ae10a0a8da5ca9b95fafdc00037b83
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2023-03-16 22:44:26 +0530

    [Ayushi] Added Address_Book table

--- AddressBook.sql
+++ AddressBook.sql
@@ -2,3 +2,10 @@
 Create Database Address_Book_Service;
 SELECT * FROM master.sys.databases;
 Use Address_Book_Service;
+
+--------------UC-2------------------
+create Table Address_Book
+(
+First_Name varchar(100), Last_Name varchar(100), Address varchar(200), 
+City varchar(50), State varchar(50), Zip_Code int, Phone_Number bigint, E_Mail varchar(50) 
+);
